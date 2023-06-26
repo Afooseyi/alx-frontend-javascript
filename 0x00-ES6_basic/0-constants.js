352a72f699dec55f654a62312a4f7b50af502167
Afooseyi <afooseyi@gmail.com>
2023-06-26 15:16:56 -0700

    constants

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+function taskFirst() {
+  const name = 'John';
+  const age = 25;
+  const country = 'USA';
+
+  console.log(`Name: ${name}`);
+  console.log(`Age: ${age}`);
+  console.log(`Country: ${country}`);
+}
+
+function taskNext() {
+  let x = 5;
+  let y = 10;
+  let sum = x + y;
+
+  console.log(`Sum: ${sum}`);
+}
